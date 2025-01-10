e29f55bc4343e7b336a513d1e76d6bd9eb8be994
Wittner Attila <wiattila8@gmail.com>
2025-01-10 22:43:28 +0100

    initial commit

---
+++ 1-20-1 - Walkers/src/main/java/com/goatwick/walkers/Walkers.java
@@ -0,0 +1,17 @@
+package com.goatwick.walkers;
+
+///// import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+///// import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(Walkers.MODID)
+public class Walkers
+{
+	public static final String MODID = "walkers";
+	
+	public Walkers() {
+		///// IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
+	}
+   
+}
